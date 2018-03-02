2464c1675d21930a24c4b51e3d6b912a13cf2b38
joelcolucci <joelcolucci@gmail.com>
2018-03-02 18:50:48 -0500

    feat: setup thunk middleware config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
 import rootReducer from './reducers';
-
 import './index.css';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+let middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(...middleware)
+  )
 );
-
 
 ReactDOM.render(
   <Provider store={store}>
