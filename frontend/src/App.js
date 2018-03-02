cdbf324e3941b796826e6cb59333a75a86cf6902
joelcolucci <joelcolucci@gmail.com>
2018-03-02 10:43:27 -0500

    refactor: simplify route view composition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import DefaultView from './views/DefaultView';
-import PostsEditorView from './views/PostsEditorView';
-
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={DefaultView} />
-        <Route exact path="/posts/create" component={PostsEditorView} />
+        <Route exact path="/" render={() => {
+          return (
+            <h2>Default view</h2>
+          );
+        }} />
+
+        <Route exact path="/posts/create" render={() => {
+          return (
+            <h2>Posts editor</h2>
+          );
+        }} />
       </div>
     );
   }
