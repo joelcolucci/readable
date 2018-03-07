46db441ad43e8d858fb8407f1eeeecc142c86f4a
joelcolucci <joelcolucci@gmail.com>
2018-03-07 15:35:24 -0500

    feat: add post vote counter to post detail view

--- frontend/src/containers/PostReadPage.js
+++ frontend/src/containers/PostReadPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import PostVoteCounter from '../components/PostVoteCounter';
 import { readPost } from '../actions/postActions';
 
 
@@ -16,6 +17,7 @@ class PostReadPage extends React.Component {
     return (
       <React.Fragment>
         <div>Post Read Page</div>
+        <PostVoteCounter postId={this.props.postId} voteCount={this.props.voteScore} />
         <h1>{this.props.title}</h1>
         <p>{this.props.author}</p>
         <div>{this.props.body}</div>
@@ -30,9 +32,7 @@ function mapStateToProps(state, ownProps) {
   let post = postsReducer.postsById[ownProps.postId] || {};
   return {
     id: post.id,
-    title: post.title || '',
-    author: post.author || '',
-    body: post.body || ''
+    ...post
   };
 }
 
