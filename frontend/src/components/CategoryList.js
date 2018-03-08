7a53c60cffd2c7c289d77a6c35556cce16d44830
joelcolucci <joelcolucci@gmail.com>
2018-03-08 16:16:23 -0500

    feat: style category-list

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -4,16 +4,22 @@ import { Link } from 'react-router-dom';
 
 function CategoryList(props) {
   return (
-    <React.Fragment>
-      <h2>Categories:</h2>
+    <div className="category-list">
+      <h2 className="category-list-heading">Categories</h2>
+      <ul className="category-list-items">
       {
         props.categories.map((category) => {
           return (
-            <Link key={category} to={`/${category}`}>{category}</Link>
+            <li key={category} className="category-list-item">
+              <Link
+                to={`/${category}`}
+                className="link">{category}</Link>
+            </li>
           );
         })
       }
-    </React.Fragment>
+      </ul>
+    </div>
   );
 }
 
